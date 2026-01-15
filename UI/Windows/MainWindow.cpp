0ed4912b047e6c01fef86af668af4ade57e5dd4d
Justin Kuchmy <JuKuchmy@gmail.com>
2026-01-15 17:35:13 +0800

    added Menubar

--- UI/Windows/MainWindow.cpp
+++ UI/Windows/MainWindow.cpp
@@ -1,12 +1,31 @@
 #include "UI/MainWindow.h"
+#include <QApplication>
+#include <QMenuBar>
+#include <QFile>
 
 MainWindow::MainWindow(QWidget *parent): QMainWindow(parent)
 {
-    setWindowTitle("");
+    setWindowTitle("Kabaw Chat Client");
     resize(1200,700);
+
+    QMenuBar *menuBar = new QMenuBar(this);
+
+    QMenu *fileMenu = menuBar->addMenu("&File");
+    fileMenu->addAction("New");
+    fileMenu->addAction("Open");
+    fileMenu->addAction("Save");
+
+    QAction *exitButton = fileMenu->addAction("Exit");
+
+    QMenu *helpMenu = menuBar->addMenu("&Help");
+    helpMenu->addAction("About");
+    
+    setMenuBar(menuBar);
+
+    connect(exitButton, &QAction::triggered, this, [this]() {QApplication::quit();});
 }
 
 MainWindow::~MainWindow()
 {
-    
+
 }
