e6666e5da2576ab4ccea2062619cfd24a6ab76f2
Justin Kuchmy <JuKuchmy@gmail.com>
2026-01-16 14:54:07 +0800

    WebSocketManager::instance()

--- main.cpp
+++ main.cpp
@@ -4,10 +4,16 @@
 #include "UI/MainWindow.h"
 
 
+#include <QWebSocket>
+#include <QTimer>
+#include "networking/WebSocketManager.h"
+
 int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
+    WebSocketManager::instance();
     MainWindow window;
     window.show();
+    
     return app.exec();
 }
